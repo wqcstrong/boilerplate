4f72247162cf407cf68f758cd00777d1e65a5f42
blucas.wu <blcuas.wu@huolala.cn>
2022-08-01 10:52:33 +0800

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,3 @@
 module.exports = {
   extends: require.resolve('@blucass/eslint-config-react'),
-  rules: {
-    'import/no-anonymous-default-export': [
-      'error',
-      {
-        allowArray: true,
-        allowArrowFunction: false,
-        allowAnonymousClass: false,
-        allowAnonymousFunction: false,
-        allowCallExpression: true,
-        allowLiteral: true,
-        allowObject: true,
-      },
-    ],
-  },
 };
