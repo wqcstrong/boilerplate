1de5499fb768221ceced254332ab77d75edcb4f7
blucas.wu <blcuas.wu@huolala.cn>
2022-09-23 14:56:58 +0800

    集成 tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import '@/assets/style/initial.css';
+import '@/initial/dayjs';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
