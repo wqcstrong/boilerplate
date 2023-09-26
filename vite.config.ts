d2a683474c9acf9d10ae6f0246fb2424b02e3d3b
blucas.wu <blucas.wu@huolala.cn>
2023-09-26 11:15:21 +0800

    大版本升级

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,12 @@
 import { defineConfig } from 'vite';
 import path from 'path';
-import { ViteEjsPlugin } from 'vite-plugin-ejs';
-import ssl from '@vitejs/plugin-basic-ssl';
 
 export default ({ command, mode }) => {
   const isProd = command === 'build';
 
   return defineConfig({
-    build: {
-      target: ['chrome61', 'firefox60', 'safari14', 'edge79', 'ios14'],
-      sourcemap: isProd ? 'hidden' : true,
-    },
-    esbuild: {
-      logOverride: {
-        'this-is-undefined-in-esm': 'silent',
-      },
-    },
     resolve: {
       alias: [{ find: '@', replacement: path.join(__dirname, './src') }],
     },
-    plugins: [ViteEjsPlugin(), ssl()],
   });
 };
